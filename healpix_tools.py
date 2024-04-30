b720c2db4c06f563c9add2ea65d7dd190ef995ca
mbcx7sh3 <mbcx7sh3@E-LOSX6JP7Q.local>
2024-04-30 12:52:44 +0100

    added vector plotter

--- healpix_tools.py
+++ healpix_tools.py
@@ -21,6 +21,9 @@ from astropy.visualization.wcsaxes.frame import EllipticalFrame
 from astropy.visualization import simple_norm, HistEqStretch
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
+from astropy.coordinates import SkyCoord
+from plotting_tools import FONT_SIZE_NORMAL
+
 from reproject import reproject_from_healpix
 from astropy.visualization import (ManualInterval,MinMaxInterval, AsinhStretch, SqrtStretch, HistEqStretch, ImageNormalize, LinearStretch)
 
@@ -104,6 +107,7 @@ class Mollview:
             pcent = float(vmin[1:]) 
             vmin = np.nanpercentile(array,pcent)
 
+        from astropy.visualization import (ManualInterval,MinMaxInterval, AsinhStretch, SqrtStretch, HistEqStretch, ImageNormalize, LinearStretch)
         
         if asinh:
             norm = ImageNormalize(array, vmin=vmin, vmax=vmax, stretch=AsinhStretch(a=0.1))
@@ -169,6 +173,15 @@ class Mollview:
         cb.ax.xaxis.set_label_position('bottom')
         cb.set_label(unit_label)
         
+    def add_loops(self):
+        """ Add synchrotron loops to mollview plot""" 
+        for k,v in loops.items():
+            lons,lats = read_regions_file(v[0], regs_dir='../ancillary_data/loops/')
+            lons, lats = greatcircleinterp(lons, lats, distance=0.1)
+    
+            self.axes.plot(lons,lats,transform=self.axes.get_transform('galactic'),lw=4,ls='--',color='k')
+            self.axes.text(v[1][0],v[1][1],k, color='k', transform=self.axes.get_transform('galactic'),ha='center')
+
 
     def imshow(self, array,vmin=None,vmax=None,cmap=None,interpolation='nearest', norm='hist'):
         """Wrapper for matplotlib imshow that allows for different normalisations""" 
@@ -241,6 +254,8 @@ class Gnomview:
     # Matplotlib info 
     axes : Axes = field(default_factory = lambda : None )
     figure : Figure = field(default_factory = lambda : None )
+
+    projection : str = 'TAN'
     
     def __post_init__(self): 
         
@@ -250,7 +265,25 @@ class Gnomview:
         self.wcs.wcs.crpix=[self.Nx//2,self.Ny//2]
         self.wcs.wcs.cdelt=self.cdelt
         self.wcs.wcs.crval=self.crval
-        self.wcs.wcs.ctype=['GLON-TAN','GLAT-TAN']
+        self.wcs.wcs.ctype=[f'GLON-{self.projection}',f'GLAT-{self.projection}']
+
+    @property
+    def min_x(self):
+        """Calculate the minimum x coordinate in degrees"""
+        return self.crval[0] - self.xwidth/2
+    @property
+    def max_x(self):
+        """Calculate the maximum x coordinate in degrees"""
+        return self.crval[0] + self.xwidth/2
+    
+    @property
+    def min_y(self):
+        """Calculate the minimum y coordinate in degrees"""
+        return self.crval[1] - self.ywidth/2
+    @property
+    def max_y(self):
+        """Calculate the maximum y coordinate in degrees"""
+        return self.crval[1] + self.ywidth/2
         
     def __call__(self, m : HealpixMap,
                  axes = None, 
@@ -293,8 +326,6 @@ class Gnomview:
         if isinstance(vmin,str):
             pcent = float(vmin[1:]) 
             vmin = np.nanpercentile(array,pcent)
-        if np.nansum(array) == 0:
-            raise ValueError('No data to plot')
         
         if isinstance(figure, type(None)):
             self.figure = pyplot.figure()
@@ -304,6 +335,11 @@ class Gnomview:
             self.axes = pyplot.subplot(111,projection=self.wcs)
         else:
             self.axes = axes 
+
+        if np.nansum(array) == 0:
+            print('No data to plot')
+            return
+
         self.img = self.imshow(array,vmin=vmin,vmax=vmax,cmap=cmap,interpolation='nearest', norm=None)
 
         lon = self.axes.coords[0]
@@ -319,6 +355,9 @@ class Gnomview:
         self.axes.coords[0].set_ticklabel_visible(False)
         self.axes.coords[1].set_ticks_visible(False)
         self.axes.coords[1].set_ticklabel_visible(False)
+    def remove_xaxis_ticks(self):
+        self.axes.coords[0].set_ticks_visible(False)
+        self.axes.coords[0].set_ticklabel_visible(False)
 
     def remove_yaxis_ticks(self):
         self.axes.coords[1].set_ticks_visible(False)
@@ -333,7 +372,8 @@ class Gnomview:
         """Add colorbar"""
         divider = make_axes_locatable(self.axes)
         cax = divider.append_axes('right', size='5%', pad=0.05, axes_class=pyplot.Axes)
-        cbar = self.figure.colorbar(self.axes.images[0], ax=self.axes, cax=cax,label=unit_label, ticks=ticks)
+        if len(self.axes.images) > 0:
+            cbar = self.figure.colorbar(self.axes.images[0], ax=self.axes, cax=cax,label=unit_label, ticks=ticks)
 
         #axins1 = inset_axes(self.axes, width='5%', height='100%', loc='upper center', 
         #            bbox_to_anchor=(0.6,0.,1,1), 
@@ -342,6 +382,25 @@ class Gnomview:
         #cb.ax.xaxis.set_ticks_position('bottom')
         #cb.ax.xaxis.set_label_position('bottom')
         #cb.set_label(unit_label)
+            
+    def add_overlay(self, lic, cmap='Greys', alpha=0.5,vmin=None,vmax=None):
+        """Add overlay to image"""
+        array, footprint = reproject_from_healpix((lic,'galactic'), self.wcs,
+                                                  shape_out=[self.Ny,self.Nx],
+                                                  nested=False,order=self.interpolation)
+        array[(array == hp.UNSEEN) | (np.abs(array) > 1e10)] = np.nan
+
+        print('PLOTTING OVERLAY')
+        print(np.nansum(array),np.nanmax(array),np.nanmin(array))
+        self.axes.imshow(array, cmap=cmap, alpha=alpha, origin='lower',vmin=vmin,vmax=vmax)
+
+    def add_contour(self, m,levels=[0.5,1],vmin=None,vmax=None,cmap=None,interpolation='nearest',linewidths=0.5,colors='k'):
+        array, footprint = reproject_from_healpix((m,'galactic'), self.wcs,
+                                                  shape_out=[self.Ny,self.Nx],
+                                                  nested=False,order=self.interpolation)
+        array[(array == hp.UNSEEN) | (np.abs(array) > 1e10)] = np.nan
+        contour = self.axes.contour(array,colors=colors,levels=levels,vmin=vmin,vmax=vmax,linewidths=linewidths)
+        return contour
 
     def contourf(self, m,vmin=None,vmax=None,cmap=None,levels=[0,1],interpolation='nearest',alpha=0.5):
         array, footprint = reproject_from_healpix((m,'galactic'), self.wcs,
@@ -408,7 +467,7 @@ class Arcview:
         self.wcs.wcs.crpix=[self.Nx//2,self.Ny//2]
         self.wcs.wcs.cdelt=self.cdelt
         self.wcs.wcs.crval=self.crval
-        self.wcs.wcs.ctype=['RA---ZEA','DEC--ZEA']
+        self.wcs.wcs.ctype=['GLON-ZEA','GLAT-ZEA']
         
     def __call__(self, m : HealpixMap,
                  axes = None, 
@@ -444,9 +503,17 @@ class Arcview:
                                                   shape_out=[self.Ny,self.Nx],
                                                   nested=False,order=self.interpolation)
         array[(array == hp.UNSEEN) | (np.abs(array) > 1e10)] =np.nan
+        if isinstance(vmax,str):
+            pcent = float(vmax[1:]) 
+            self.vmax = np.nanpercentile(array,pcent)
+        else:
+            self.vmax = vmax
+        if isinstance(vmin,str):
+            pcent = float(vmin[1:]) 
+            self.vmin = np.nanpercentile(array,pcent)
+        else:
+            self.vmin = vmin
 
-        if np.nansum(array) == 0:
-            raise ValueError('No data to plot')
         
         if isinstance(figure, type(None)):
             self.figure = pyplot.figure()
@@ -454,18 +521,24 @@ class Arcview:
             self.figure = figure 
         if isinstance(axes, type(None)):    
             self.axes = pyplot.subplot(111,projection=self.wcs,frame_class=EllipticalFrame)
+            self.axes.coords.frame.set_frame_shape('circle')
         else:
             self.axes = axes 
-        self.img = self.imshow(array,vmin=vmin,vmax=vmax,cmap=cmap,interpolation='bilinear', norm=None)
+
+        if np.nansum(array) == 0:
+            print('No data to plot')
+            return
+
+        self.img = self.imshow(array,vmin=self.vmin,vmax=self.vmax,cmap=cmap,interpolation='nearest', norm=None)
         # Clip the image to the frame
         self.img.set_clip_path(self.axes.coords.frame.patch)
 
         lon = self.axes.coords[0]
         lat = self.axes.coords[1]
-        lon.set_ticks_visible(False)
-        lon.set_ticklabel_visible(False)
-        lat.set_ticks_visible(False)
-        lat.set_ticklabel_visible(False)
+        #lon.set_ticks_visible(False)
+        #lon.set_ticklabel_visible(False)
+        #lat.set_ticks_visible(False)
+        #lat.set_ticklabel_visible(False)
         #lat.set_ticks([1600]*units.degree)
         #lon.set_axislabel('Galactic Longitude')
         #lat.set_axislabel('Galactic Latitude')
@@ -478,22 +551,26 @@ class Arcview:
         self.axes.coords[0].set_ticklabel_visible(False)
         self.axes.coords[1].set_ticks_visible(False)
         self.axes.coords[1].set_ticklabel_visible(False)
-        
+    def remove_yaxis_ticks(self):
+        self.axes.coords[1].set_ticks_visible(False)
+        self.axes.coords[1].set_ticklabel_visible(False)
+
     def add_grid(self, color='k'):
         """Add grid to image"""
         self.axes.coords.grid(color=color)
-        self.axes.coords['glon'].set_ticklabel(color=color)
+        self.axes.coords[0].set_ticklabel(color=color)
 
     def add_colorbar(self, unit_label=' ',  ticks=None):
         """Add colorbar"""
         
         axins1 = inset_axes(self.axes, width='5%', height='100%', loc='upper center', 
-                    bbox_to_anchor=(0.6,0.,1,1), 
+                    bbox_to_anchor=(0.7,0.,1,1), 
                     bbox_transform=self.axes.transAxes)
-        cb = self.figure.colorbar(self.img,cax=axins1,orientation='vertical',ticks=ticks)
-        #cb.ax.xaxis.set_ticks_position('bottom')
-        #cb.ax.xaxis.set_label_position('bottom')
-        cb.set_label(unit_label)
+        if hasattr(self, 'img'):
+            cb = self.figure.colorbar(self.img,cax=axins1,orientation='vertical',ticks=ticks)
+            #cb.ax.xaxis.set_ticks_position('bottom')
+            #cb.ax.xaxis.set_label_position('bottom')
+            cb.set_label(unit_label)
 
     def contourf(self, m,levels=[0.5,1],vmin=None,vmax=None,cmap=None,interpolation='nearest'):
         array, footprint = reproject_from_healpix((m,'galactic'), self.wcs,
@@ -535,3 +612,56 @@ class Arcview:
             else:
                 norm_module = simple_norm(array,norm)
         return array, vmin, vmax, norm_module
+    
+
+class VectorPlotter:
+    """
+    Reads in a text file of format:
+    # l,b 
+    l0 b0
+    l1 b1
+    ... 
+    """
+    def __init__(self, ax, wcs,region_data):
+        self.ax = ax
+        self.region_data = region_data
+        self.wcs = wcs
+
+    def plot_regions(self, linestyle='--', color='black', linewidth=1,alpha=0.7):
+        for region in self.region_data:
+            # Extract region parameters from the dictionary
+            txt_file = region['txt_file']
+            linestyle = region.get('linestyle', '--')
+            color = region.get('color', 'black')
+            l_center = region.get('l_center', 0)
+            b_center = region.get('b_center', 0)
+            data = np.loadtxt(txt_file,delimiter=' ')
+
+            # Create a Rectangle patch representing the region
+            xlim = self.ax.get_xlim()
+            ylim = self.ax.get_ylim()
+            for istart,iend in zip(range(0,data.shape[0]-1),range(1,data.shape[0])):
+                self.ax.plot([data[istart,0],data[iend,0]],
+                             [data[istart,1],data[iend,1]],
+                             linestyle=linestyle,
+                                color=color,
+                             linewidth=linewidth,
+                             transform=self.ax.get_transform('galactic'))
+            self.ax.set_xlim(xlim)
+            self.ax.set_ylim(ylim)
+
+
+            # Extract label parameters from the dictionary
+            label = region.get('label', '')
+            label_l = region.get('label_l', l_center)
+            label_b = region.get('label_b', b_center)
+            label_color = region.get('label_color', 'black')
+
+
+            # Convert label coordinates to pixel coordinates using WCS
+            label_coord = SkyCoord(l=label_l*u.deg, b=label_b*u.deg, frame='galactic')
+            label_pixel = self.wcs.world_to_pixel(label_coord)
+
+            # Add label to the region if provided
+            if label:
+                self.ax.text(label_pixel[0], label_pixel[1], label, color=label_color, fontsize=FONT_SIZE_NORMAL, ha='center', va='center')
